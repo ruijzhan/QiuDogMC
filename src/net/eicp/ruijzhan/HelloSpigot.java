8d9c43125e54e33404125e710b917f992181c82b
Ruijian Zhang <ruijian.zhang@nokia.com>
2016-06-20 16:52:43 +0800

    Created project and first class

---
+++ src/net/eicp/ruijzhan/HelloSpigot.java
@@ -0,0 +1,16 @@
+package net.eicp.ruijzhan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HelloSpigot extends JavaPlugin {
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override 
+	public void onDisable(){
+		
+	}
+
+}
